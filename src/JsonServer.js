90b2b66ddbe16dfe3b71a093ef5d26dd3ce32002
msnblr0909 <msnblr0909@gmail.com>
2023-06-12 17:58:25 +0530

    12/06 Code changes added

--- src/JsonServer.js
+++ src/JsonServer.js
@@ -4,14 +4,18 @@ import "./Json.css";
 function JsonServer() {
   const [data, setData] = useState("");
   const [name, setName] = useState("");
-  const [mobile,setMobile]=useState("");
+  const [mobile, setMobile] = useState("");
+  const [party, setParty] = useState("");
   useEffect(() => {
     getUser();
   }, []);
-  function getUser(){
+  function getUser() {
     fetch("http://localhost:4000/users").then((result) => {
       result.json().then((resp) => {
         setData(resp);
+        setName(resp[0].name);
+        setMobile(resp[0].mobile);
+        setParty(resp[0].party);
         // console.log(resp);
       });
     });
@@ -19,7 +23,8 @@ function JsonServer() {
   function Handler(e) {
     console.log({ name });
     alert("successfull");
-    let data1 = { name,mobile };
+    let data1 = { name, mobile, party };
+    //Create user list
     fetch("http://localhost:4000/users", {
       method: "POST",
       headers: {
@@ -37,69 +42,112 @@ function JsonServer() {
     //   alert(abc);
     // }
   }
-  function deleteUsers(id)
-  {
-fetch(`http://localhost:4000/users/${id}`,{
+  //Delete  user list
+  function deleteUsers(id) {
+    fetch(`http://localhost:4000/users/${id}`, {
+      method: "DELETE",
+    }).then((result2) => {
+      result2.json().then((resp2) => {
+        console.warn(resp2);
+        getUser();
+      });
+    });
+  }
+  function selectUsers(id) {
+   console.warn(data[id-1])
+    let item1 = (data[id-1]);
+    setName(item1.name);
+    setMobile(data[id-1].mobile);
+    setParty(data[id-1].party);
 
-method:"DELETE"
-  }).then((result2)=>{
-  result2.json().then((resp2)=>{
-console.warn(resp2);
-getUser();
-  })
-})
-}
+  }
 
-  
   return (
     <>
-
-    <h1>MSN REGISTER TABLE</h1>
-      <div className="reg_form">
-        <label>Name:</label>
-        <input className="inpt"
-          type="name"
-          name="name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <br/> <br/>
-          <label>Mobile:</label>
-        <input className="inpt-one"
-          type="name"
-          name="name"
-          value={mobile}
-          onChange={(e) => {
-            setMobile(e.target.value);
-          }}
-        />
-        <br />
-        <br />
-        <button style={{position: "relative",
-    /* right: -131px; */
-    left: "157px", top:"-13px",
-    width: "90px"}} onClick={Handler}>Submit</button>
-      </div>
-      <div className="tblstyle">
-        <table border="1">
-          <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Mobile</td>
-            <td>Operation</td>
-          </tr>
-          {data &&
-            data.map((item,i) => 
-              <tr key={i}>
-                <td>{item.id}</td>
-                <td>{item.name}</td>
-                <td>{item.mobile}</td>
-                <td><button onClick={()=>deleteUsers(item.id)}>Delete</button></td>
-              </tr>
-            )}
-        </table>
+      <h1>MSN REGISTER TABLE</h1>
+      <div className="reg_box">
+        <div className="reg_form">
+          <h4>Registration Form</h4>
+          <label style={{ padding: "10px" }}>Name:</label>
+          <input
+            className="inpt"
+            type="name"
+            name="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <br /> <br />
+          <label style={{ padding: "10px" }}>Mobile:</label>
+          <input
+            className="inpt-one"
+            type="name"
+            name="name"
+            value={mobile}
+            onChange={(e) => {
+              setMobile(e.target.value);
+            }}
+          />
+          <br /> <br />
+          <label style={{ padding: "10px" }}>Party:</label>
+          <input
+            style={{ position: "relative", left: "12%", padding: "3px" }}
+            type="name"
+            name="name"
+            value={party}
+            onChange={(e) => {
+              setParty(e.target.value);
+            }}
+          />
+          <br />
+          <br />
+          <br />
+          <br />
+          <button
+            style={{
+              position: "relative",
+              /* right: -131px; */
+              left: "157px",
+              top: "-13px",
+              width: "90px",
+            }}
+            onClick={Handler}
+          >
+            Submit
+          </button>
+        </div>
+        <div className="tblstyle">
+          <table border="1">
+            <tr>
+              <td>ID</td>
+              <td>Name</td>
+              <td>Mobile</td>
+              <td>Operation</td>
+              <td>Party</td>
+            </tr>
+            {data &&
+              data.map((item, i) => (
+                <tr key={i}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.mobile}</td>
+                  <td>{item.party}</td>
+                  <td>
+                    <button onClick={() => deleteUsers(item.id)}>Delete</button>
+                  </td>
+                  <td>
+                    <button onClick={() => selectUsers(item.id)}>Update</button>
+                  </td>
+                </tr>
+              ))}
+          </table>
+        </div>
+        <div className="select-box">
+          <input type="text" value={name} />
+          <input type="text" value={mobile} />
+          <input type="text" value={party} />
+        </div>
       </div>
     </>
   );
